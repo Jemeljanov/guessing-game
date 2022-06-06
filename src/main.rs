deee1ff49419a455a996c0a8774bc331315acc2b
Jemeljanov <jemeljanov.dmitri@gmail.com>
2022-06-06 19:08:40 +0200

    added random number

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Guess the number:");
+    println!("Guess the number (from 1 to 10):");
+
+    let secret_number = rand::thread_rng().gen_range(1,10);
+    println!("secret number is {}", secret_number);
+
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
         .expect("Failed to readline");
     println!("you guessed {}", guess);
+
 }
